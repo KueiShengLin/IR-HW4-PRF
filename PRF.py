01cfffcaf9d21f54e0444375ac587c60680f475b
Tim Lin <tarry21304@gmail.com>
2017-11-08 11:18:20 +0800

    Rocchio down but TMM I don't know why it can't work

--- PRF.py
+++ PRF.py
@@ -15,11 +15,11 @@ BG = []
 
 ROCCHIO_ALPHA = 0.7
 ROOCHIO_BETA = 0.3
-TMM_ALPHA = 0.4
+TMM_ALPHA = 0.3
 TMM_BETA = 0.3
-KL_A = 0.3
-KL_B = 0.3
-KL_D = 0.6
+KL_A = 0.5
+KL_B = 0.0
+KL_D = 0.7
 
 RANKING = []
 
@@ -107,7 +107,7 @@ def TMM(first):
 
     tmm_list = copy.deepcopy(relevant_doc_word)
 
-    for iteration in range(1, 20):
+    for iteration in range(1, 21):
         print(iteration)
         for q in range(len(relevant_doc_word)):  # query 800
             tmm = copy.deepcopy(tmm_list[q])
@@ -119,6 +119,7 @@ def TMM(first):
                     tmm[tmm_voc] /= tmm_total
             tmmwd = copy.deepcopy(relevant_doc[q])  # 只是剛好relevant_doc[q] = q all file dictionary
             tdwd = copy.deepcopy(relevant_doc[q])   # 同上
+
             # E_Step
             for doc_id, doc in enumerate(relevant_doc[q]):
                 doc_total = sum(doc.values())
@@ -130,6 +131,7 @@ def TMM(first):
                     twd_deno = ((tmm[word] * (1 - TMM_ALPHA - TMM_BETA)) + (pwd * TMM_ALPHA) + (pbg * TMM_BETA))
                     tmmwd[doc_id][word] = (tmm[word] * (1 - TMM_ALPHA - TMM_BETA)) / twd_deno
                     tdwd[doc_id][word] = (pwd * TMM_ALPHA) / twd_deno
+
             # M_Step
             tmm_molecular_list = []
             for word_id, word in enumerate(tmm):
@@ -170,30 +172,34 @@ def KL(tmm_query):
             print(q_id)
 
         kl_list = []
-        tmq = tmm_query[q_id]
+        tmq = copy.deepcopy(tmm_query[q_id])
 
         q_total = sum(q.values())
         new_q = copy.deepcopy(q)
-        for q_word in q:
-            tmm, pbg = 0, 0
+        new_q.update(tmq)
+
+        for q_word in new_q:
+            base_q, tmm, pbg = 0, 0, 0
+            new_q[q_word] = 0
+
             if q_word in tmq:
                 tmm = tmq[q_word]
+            if q_word in q:
+                base_q = q[q_word]
             if int(q_word) < len(BG):
                 pbg = math.exp(BG[int(q_word)])
-            new_q[q_word] = (KL_A * q[q_word] / q_total) + (KL_B * tmm) + ((1 - KL_A - KL_B) * pbg)
 
+            new_q[q_word] = (KL_A * base_q / q_total) + (KL_B * tmm) + ((1 - KL_A - KL_B) * pbg)
         #KL
         for doc in DOCUMENT:
             doc_total = sum(doc.values())
-            # new_d = copy.deepcopy(doc)
-            # for word in doc:
-            #     pbg = 0
-            #     if int(word) < len(BG):
-            #         pbg = math.exp(BG[int(word)])
-            #
-            #     new_d[word] = (KL_D * doc[word] / doc_total) + ((1 - KL_D) * math.exp(BG[int(word)]))
-            kl_score = -sum((new_q[q_word]) * math.log10(KL_D * doc[q_word] / doc_total) + ((1 - KL_D) * math.exp(BG[int(q_word)]))
-                            for q_word in q if q_word in doc)
+            # kl_score = -sum(new_q[q_word] * math.log((KL_D * doc[q_word] / doc_total) + ((1 - KL_D) * math.exp(BG[int(q_word)])), math.e)
+            #                 for q_word in new_q if q_word in doc)
+            kl_score = 0
+            for q_word in new_q:
+                if q_word in doc:
+                    new_doc = (KL_D * doc[q_word] / doc_total) + ((1 - KL_D) * math.exp(BG[int(q_word)]))
+                    kl_score -= (new_q[q_word]) * math.log10(new_doc)
             kl_list.append(kl_score)
 
         #sorting
@@ -205,9 +211,50 @@ def KL(tmm_query):
         RANKING.append(q_rank)
 
 
-def rocchio(first):
+def QL(tmm_query):
+    global QUERY, DOCUMENT, RANKING, DOC_NAME, BG
+    global KL_A, KL_B, KL_D
+
+    for q_id, q in enumerate(QUERY):
+        if q_id % 100 == 0:
+            print(q_id)
+
+        tmq = copy.deepcopy(tmm_query[q_id])
+        q_total = sum(q.values())
+        new_q = copy.deepcopy(q)
+        new_q.update(tmq)
+
+        for q_word in new_q:
+            base_q, tmm, pbg = 0, 0, 0
+            new_q[q_word] = 0
+
+            if q_word in tmq:
+                tmm = tmq[q_word]
+            if q_word in q:
+                base_q = q[q_word]
+            if int(q_word) < len(BG):
+                pbg = math.exp(BG[int(q_word)])
+            new_q[q_word] = (KL_A * base_q / q_total) + (KL_B * tmm) + ((1 - KL_A - KL_B) * pbg)
+
+        score_list = []
+        for doc in DOCUMENT:
+            doc_total = sum(doc.values())
+            score = 1
+            for q_word in new_q:
+                if q_word in doc:
+                    score *= ((0.7 * doc[q_word] / doc_total) + (0.3 * math.exp(BG[int(q_word)])))
+            score_list.append(score)
+
+        sort = sorted(score_list, reverse=True)
+        q_rank = []
+        for sort_num in sort:
+            q_rank.append(DOC_NAME[score_list.index(sort_num)])
+
+        RANKING.append(q_rank)
+
+
+def relevant_doc(first):
     global QUERY
-    global ROCCHIO_ALPHA, ROOCHIO_BETA
     relevant_doc = []
     relevant_doc_word = []
     new_q = []
@@ -216,30 +263,33 @@ def rocchio(first):
         doc_dict = []
         total_voc = {}
         for d_name in d_list:
-            temp = DOCUMENT[DOC_NAME.index(d_name)]
+            temp = copy.deepcopy(DOCUMENT[DOC_NAME.index(d_name)])
             doc_dict.append(temp)
-            total_voc.update(temp)
+            for temp_word in temp:
+                if temp_word not in total_voc:
+                    total_voc[temp_word] = temp[temp_word]
+                else:
+                    total_voc[temp_word] += temp[temp_word]
+
         relevant_doc.append(doc_dict)     # query k 的 relevant doc
         relevant_doc_word.append(total_voc)     # query k relevant doc all word
 
-    for q_id, q in enumerate(QUERY):
-        pseudo = q.copy()
-        for word in pseudo:
-            pseudo[word] *= ROCCHIO_ALPHA
-
-        for doc in relevant_doc[q_id]:
-            for relevant_word in doc:
-                if relevant_word not in pseudo:
-                    pseudo[relevant_word] = doc[relevant_word] * ROOCHIO_BETA * (1 / len(relevant_doc))
-                else:
-                    pseudo[relevant_word] += doc[relevant_word] * ROOCHIO_BETA * (1 / len(relevant_doc))
-        new_q.append(pseudo)
-    return new_q
+    # for q_id, q in enumerate(QUERY):
+    #     pseudo = q.copy()
+    #
+    #     for doc in relevant_doc[q_id]:
+    #         for relevant_word in doc:
+    #             if relevant_word not in pseudo:
+    #                 pseudo[relevant_word] = doc[relevant_word]
+    #             else:
+    #                 pseudo[relevant_word] += doc[relevant_word]
+    #     new_q.append(pseudo)
+    return relevant_doc_word
 
 
-def writefile():
+def writefile(name):
     global QUERY_NAME, DOC_NAME, RANKING
-    with open('ican.txt', 'w') as retrieval_file:
+    with open(name + '.txt', 'w') as retrieval_file:
         retrieval_file.write("Query,RetrievedDocuments\n")
         for retrieval_id, retrieval_list in enumerate(RANKING):
             retrieval_file.write(QUERY_NAME[retrieval_id] + ',')
@@ -250,20 +300,26 @@ def writefile():
 
 
 readfile()
-# VSM_re = VSM(doc_name=DOC_NAME, query_name=QUERY_NAME, document=DOCUMENT, query=QUERY, rank_amount=5)
+# VSM_re = VSM(doc_name=DOC_NAME, query_name=QUERY_NAME, document=DOCUMENT, query=QUERY, rank_amount=1)
 # VSM_re.calculate()
 # VSM_re.writeAns('VSM5')
 print('VSM down')
-answer = ans_read('VSM5.txt')
-tmm_query = TMM(answer)
-print('TMM down')
-KL(tmm_query)
-print('KL down')
-writefile()
-# new_que = rocchio(answer)
-# VSM_ro = VSM(doc_name=DOC_NAME, query_name=QUERY_NAME, document=DOCUMENT, query=new_que, rank_amount=100)
-# VSM_ro.calculate()
-# VSM_ro.writeAns('rotest')
+answer = ans_read('VSM\\VSM1.txt')
+# tmm_query = TMM(answer)
+# print('TMM down')
+# KL(tmm_query)
+# writefile('KL')
+# print('KL down')
+
+# QL(tmm_query)
+# writefile('QL')
+# print('QL down')
+
+
+new_que = relevant_doc(answer)
+VSM_ro = VSM(doc_name=DOC_NAME, query_name=QUERY_NAME, document=DOCUMENT, query=QUERY, rank_amount=100)
+VSM_ro.rocchioauto(0.6, 0.4, new_que, 1)
+VSM_ro.writeAns('roteee2')
 
 
 print('Process down')
